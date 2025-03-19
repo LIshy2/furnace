7325ce6faed345e721f012217680592775c28131
Islam Talipov <ishaykhiev2017@litsey2.ru>
2025-03-20 00:38:02 +0300

    grammar rename

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,27 +2,27 @@ pub mod ast;
 
 use lalrpop_util::lalrpop_mod;
 
-lalrpop_mod!(pub cubicaltt, "/parser/cubicaltt.rs");
+lalrpop_mod!(pub grammar, "/parser/grammar.rs");
 
 mod tests {
-    use crate::parser::cubicaltt;
+    use crate::parser::grammar;
     #[test]
     fn decl() {
-        cubicaltt::DeclParser::new()
+        grammar::DeclParser::new()
             .parse("isAssociative (M : U) (op : M -> M -> M) : U = (a b c : M) -> Path M (op a (op b c)) (op (op a b) c)").unwrap();
     }
 
     #[test]
     fn module() {
-        cubicaltt::ModuleParser::new()
+        grammar::ModuleParser::new()
             .parse(
-                "module idtypes where { import sigma; import univalence a = a }",
+                "module idtypes where { import sigma; import univalence a: U = a }",
             )
             .unwrap();
     }
     #[test]
     fn system() {
-        cubicaltt::SystemParser::new()
+        grammar::SystemParser::new()
             .parse(
                 "[-> a]"
             ).unwrap();
